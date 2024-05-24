d0b5768b9141db36225f14143c19761a2e62bdc4
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-05-24 12:34:45 +0530

    asjkjasbjdd

--- excel_summary/settings.py
+++ excel_summary/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
      'whitenoise.middleware.WhiteNoiseMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'excel_summary.urls'
